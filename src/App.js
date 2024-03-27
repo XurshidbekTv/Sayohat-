946ca24e9b687ad54adf73c9b22d67ae9fa4fb46
XurshidbekTV <xurshidbektursunaliyev@gmail.com>
2024-03-27 16:04:43 +0500

    date chelenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter/>
     </div>
   );
 }
 
+function Counter(){
+const [count, setCount]=useState(0);
+const [step, setStep]=useState(1);
+const date=new Date("june 21 2027");
+date.setDate(date.getDate()+count);
+  return(
+    <div className='box'>
+       <div>
+        <button className='fw-bold px-5 rounded btn btn-primary m-3' onClick={()=>setStep((c)=>c-1)}>-</button>
+        <span className='fw-bold fs-5'>Step:{step}</span>
+        <button className='fw-bold px-5 rounded btn btn-primary m-3' onClick={()=>setStep((c)=>c+1)}>+</button>
+      </div>
+      <div>
+        <button className='fw-bold px-5 rounded btn btn-primary m-3' onClick={()=>setCount((c)=>c-step)}>-</button>
+        <span className='fw-bold fs-5'>Count:{count}</span>
+        <button className='fw-bold px-5 rounded btn btn-primary m-3' onClick={()=>setCount((c)=>c+step)}>+</button>
+      </div>
+      <div className='display-5 fw-bold'>
+      <span>{count===0 ? "Today is " :count>0 ? `${count} days from today is `: count<0  `${Math.abs(count)} days ago was ` }</span>
+      <span>{date.toDateString()}</span>
+      </div>
+    </div>
+  )
+}
 export default App;
